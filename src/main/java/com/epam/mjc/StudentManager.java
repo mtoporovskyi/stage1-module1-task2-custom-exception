ef8af61faf56a7f339ea24dfef29fe7037c117db
Myroslav Toporovskyi <mtoporovskyy@gmail.com>
2023-12-27 16:25:24 +0200

    stage1-module1-task2-custom-exception

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,8 +5,12 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws StudentException {
+    if (Student.getValueOf(studentID) != null) {
+      return Student.getValueOf(studentID);
+    } else {
+      throw new StudentException("Could not find student with ID " + studentID);
+    }
   }
 
   public static void main(String[] args) {
@@ -16,6 +20,5 @@ public class StudentManager {
       Student student = manager.find(IDs[i]);
       System.out.println("Student name " + student.getName());
     }
-
   }
 }
